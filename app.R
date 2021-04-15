8fd17d0ead35b02555a5e6b072d2fe4a6d5e58f0
Mateus Reis <mateussdreis@gmail.com>
2021-04-15 17:04:03 -0300

    Aqui foram intrdouzidos as listas de opção com o radio butons e como inserir imagem´

--- app.R
+++ app.R
@@ -10,15 +10,22 @@ ui <- fluidPage(
   #sidebarlayout cria o layout da página
   sidebarLayout(
     # sidebarPanel campos de entrada e botões
-    sidebarPanel(h2("Nessa area ficaro os dados de input"),
+    sidebarPanel(h2("Entrada de dados"),#Nessa area ficaro os dados de input
+                 tags$img(src="logoR.png", height=100, width=100), #atente-se para a necessidade da pasta www e a duplicata da imagem
                  textInput("idEntradaTexto","Informe um texto"),#entrada de strings
-                 numericInput("idEntradaNumerica","Informe um número", 0, min = 0, max = 100, step = 5)
+                 numericInput("idEntradaNumerica","Informe um número", 0, min = 0, max = 100, step = 5),
+                 dateInput("idData","Informe a Data",format = "dd-mm-yyyy", language = "pt", autoclose = T),
+                 radioButtons("idRadio","Selecione seu sexo", choices = c("M","F"), inline = T),#radio buton
+                 radioButtons("idRadio1","Selecione seu sexo", list("Opcao 1"=1,"Opcao 2"=2,"Opcao 3"=3),selected = 1, inline = F)#radio buton
     ), #h tamanho dos textos
     
     mainPanel(
-      h2("Aqui serao os elementos de saída de dados"),
+      h2("Saída de dados"), #Aqui serao os elementos de saída de dados
       textOutput("idSaidaTexto"),
-      verbatimTextOutput("idSaidaNumerica",placeholder = TRUE)#para dados numéricos!
+      verbatimTextOutput("idSaidaNumerica",placeholder = TRUE),#para dados numéricos!
+      verbatimTextOutput("idSaidaData"),
+      verbatimTextOutput("idOpcaoSelecionada"),
+      verbatimTextOutput("idOpcaoSelecionada1")
     ),
   )
 )
@@ -28,7 +35,10 @@ server <- function(input, output) {
   output$idSaidaTexto<-renderText({input$idEntradaTexto})
   output$idSaidaNumerica<-renderText({input$idEntradaNumerica})
   #saída recebe o output e o identificador da saída, renderText serve para atualizar automaticamente a saída
-}
+  output$idSaidaData<-renderText({as.character(input$idData)})
+  output$idOpcaoSelecionada<-renderText({input$idRadio})
+  output$idOpcaoSelecionada1<-renderText({input$idRadio1})
+  }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
